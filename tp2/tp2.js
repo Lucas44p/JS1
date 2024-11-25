7c664b61f4e7ec4b67d05de38a2942d8785b0fd3
Lucas <lucasguilloteau33@gmail.com>
2024-11-25 15:30:02 +0100

    fc

--- tp2/tp2.js
+++ tp2/tp2.js
@@ -1,19 +1,32 @@
-function calcul(){
-    // lire le dom
-    const km = document.getElementById('km').value
-    const litre = document.getElementById('litre').value
-    document.getElementById('km').value =''
-    document.getElementById('litre').value=''
-    const conso = (litre *100)/km
-    // afficher dans le DOM
-    document.getElementById('conso').innerHTML=conso.toFixed(1) = 'l/100km'
-    let tranche =''
-    if ( conso <7){
-        tranche = 'normal'
-    }else if (conso < 9){// 7 <conso< 9
-        tranche ='gourmand'
-    } else{
-        tranche='SUV de luxe'
+function calculer()
+{
+
+    const litres = parseFloat(document.getElementById('litres').value);
+    const kilos = parseFloat(document.getElementById('kilos').value);
+    document.getElementById('litres').value = ''
+    document.getElementById('kilos').value = ''
+    const alert = document.getElementById('alert')
+
+    let status = "";
+    let couleur = "";
+
+    const conso = 100*litres/kilos;
+
+    if(conso < 5.5){
+        status = "Économique";
+        couleur ="alert alert-success mt-4"
+    } else if( conso< 7.5){
+        status = "Normal"
+        couleur = "alert alert-warning mt-4"
+    }else {
+        status = "Danger"
+        couleur = "alert alert-danger mt-4"
     }
-    document.getElementById('conseil').innerHTML =tranche;
+
+    document.getElementById('conso').innerHTML = conso;
+    document.getElementById('status').innerHTML = status;
+    alert.className=couleur
+
+
+
 }
